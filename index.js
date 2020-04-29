8e9d7ea36fdd8f5adad0145ba59966938c277771
Romanova Elena <for-alisia@yandex.ru>
2020-04-29 18:58:32 +0500

    Added hbs engine

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
 
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`)
