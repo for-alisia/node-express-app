bef146b728d45a1fb02860678a3fd7c5b03a8dbd
Romanova Elena <for-alisia@yandex.ru>
2020-05-06 14:18:26 +0500

    Fixed bugs (cart page)

--- public/scripts.js
+++ public/scripts.js
@@ -1,8 +1,12 @@
-document.querySelectorAll('.price').forEach( el => {
-    el.textContent = Intl.NumberFormat('ru-Ru', {
+function toCurrency(price) {
+    return new Intl.NumberFormat('ru-Ru', {
         currency: 'rub',
         style: 'currency'
-    }).format(el.textContent);
+    }).format(price);
+
+}
+document.querySelectorAll('.price').forEach( el => {
+    el.textContent = toCurrency(el.textContent);   
 });
 
 const cart = document.querySelector('#cart');
@@ -15,8 +19,26 @@ if (cart) {
             fetch(`/cart/remove/${id}`, {
                 method: 'delete'
             }).then(res => res.json())
-              .then(cart => {
-                  console.log(cart);
+              .then(cartContent => {
+                  console.log(cartContent);
+
+                  if (cartContent.courses.length) {
+                      const html = cartContent.courses.map( el => {
+                          return `
+                        <tr>
+                            <td>${el.title}</td>
+                            <td>${el.count}</td>
+                            <td class="price">${toCurrency(el.price)}</td>
+                            <td><button class="btn btn-primary remove-item" data-id="${el.id}">Delete</button></td>
+                        </tr>
+                          `
+                      }).join('');
+                      const tbody = document.querySelector('tbody');
+                      tbody.innerHTML = html;
+                      document.getElementById('total').textContent = toCurrency(cartContent.price);
+                  } else {
+                    cart.innerHTML = '<p>Your cart is empty now. Look at our <a href="/courses">awesome courses</a></p>'
+                  }
               })
         }
     });
